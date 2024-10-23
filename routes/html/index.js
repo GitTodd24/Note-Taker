5f2aa8c6d429ab0b2ec1b08a80f73d483aedabb2
GitTodd24 <todd.asante@gmail.com>
2024-10-23 01:30:49 -0500

    Installed Postgres 17.0

---
+++ routes/html/index.js
@@ -0,0 +1,14 @@
+const router=require("express").Router()
+const path=require("path");
+
+
+
+router.get("/notes", (req, res)=>{})
+
+
+router.get("*", (req, res)=>{})
+
+
+
+
+module.exports=router;
